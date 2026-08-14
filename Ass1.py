8ad2361a9ff437bab0f3182415133b098f27a0aa
vshalsah29 <vshalsah29@gmail.com>
2026-08-14 11:06:14 +0530

    1st assig

---
+++ Ass1.py
@@ -0,0 +1,8 @@
+# Assignment 1: Parity [Warm-up]
+# Return 'even' or 'odd' for integer n.
+def parity(n):
+     if n % 2 == 0:
+         return 'even'
+     else:
+         return 'odd'
+print(parity(10))
