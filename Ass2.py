8ca832679ffb03607ca1df673fe24becf8c8e164
vshalsah29 <vshalsah29@gmail.com>
2026-08-14 11:14:30 +0530

    2nd assig

---
+++ Ass2.py
@@ -0,0 +1,8 @@
+# Assignment 2: Absolute Value [Warm-up]
+# Return |n| without using abs().
+def absolute_value(n): 
+    if n < 0:
+        return -n
+    else:
+        return n    
+print(absolute_value(-10))  # Output: 5
